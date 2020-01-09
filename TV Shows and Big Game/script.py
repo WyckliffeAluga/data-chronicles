2be592557a1fe7936d545472b84df95d2edd9181
WyckliffeAluga <wyckliffe@culturerobotics.com>
2020-01-08 19:10:43 -0800

    New project added

---
+++ TV Shows and Big Game/script.py
@@ -0,0 +1,55 @@
+
+# hi
+# in this project I will try to analyze tv data to answer questions like
+# What are the most extreme outcomes in super bowl
+# How does the game affect tv viewership
+# how have viewership , TV ratings and ad cost evolved over time
+# Who is the most prolific musician in terms of half time perfomances
+
+# import modules
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# style the plots
+plt.style.use('seaborn')
+
+# Load all csv data
+superBowls = pd.read_csv('datasets/super_bowls.csv')
+tv         = pd.read_csv('datasets/tv.csv')
+halftimeMusicians = pd.read_csv('datasets/halftime_musicians.csv')
+
+# display sample data
+print(superBowls.head(5))
+print(tv.head(5))
+print(halftimeMusicians.head(5))
+
+# inspect tha data for missing values
+
+# summary of tv data
+print(tv.info())
+
+print('/n')
+
+# summary of halftime musician's data
+print(halftimeMusicians.info())
+
+# visually inspect super bowl data
+
+# plot a histogram of combined points
+superBowls.combined_pts.hist()
+plt.xlabel('combined points')
+plt.ylabel('number of super bowls')
+plt.show()
+
+# display the super bowls with the highest and lowest combined points
+print(superBowls[superBowls['combined_pts'] > 70])
+print(superBowls[superBowls['combined_pts'] < 25])
+
+# let us play around with point difference distribution
+
+# plot a histogram of point differences
+plt.hist(superBowls.difference_pts)
+plt.xlabel('point difference')
+plt.ylabel('number of super bowls')
+plt.show()
