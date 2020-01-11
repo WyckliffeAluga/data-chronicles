072b77ff57c1944cfb6c9791db92270e01854956
WyckliffeAluga <wyckliffe@culturerobotics.com>
2020-01-10 22:00:54 -0800

    update the script

---
+++ Up and Down with the Kardashians/script.py
@@ -0,0 +1,14 @@
+
+
+# alright well I am not a big fan nor a hater of the Kardashians and jenners
+# the family polarization has an intriguing marketing prowess.
+# say what you want about them but whatever they touch turns into content and eventul viral
+
+# I will try to explore the data underneath the hype they create and find who is actually more famous
+
+# I will use Google Trends data
+
+# load the pandas
+import pandas as pd
+
+# read the dataset 
