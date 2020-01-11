1ba6894e352e165cd0d047ae718ef93d08023aaa
WyckliffeAluga <wyckliffe@culturerobotics.com>
2020-01-10 22:07:28 -0800

    Update script.py

--- Up and Down with the Kardashians/script.py
+++ Up and Down with the Kardashians/script.py
@@ -16,3 +16,15 @@ trends = pd.read_csv('datasets/trends_kj_sisters.csv')
 
 # inspect a sample
 print(trends.head())
+
+# Numbers represent search interest relative to the highest point on the chart for the given region and time.
+# A value of 100 is the peak popularity for the term.
+# A value of 50 means that the term is half as popular.
+# A score of 0 means there was not enough data for this term.
+# well that is not straight foward
+
+# first let us get some better column names
+trends.columns = ['month', 'kim', 'khloe','kourtney','kendall','kylie']
+
+# check
+print(trends.head())
