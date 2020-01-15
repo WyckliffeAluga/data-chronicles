7bee905458648c47ec1046c8255533e61c480be7
WyckliffeAluga <wyckliffe@culturerobotics.com>
2020-01-15 11:13:44 -0800

    add first name as a column

--- Gender Prediction using Sound/script.py
+++ Gender Prediction using Sound/script.py
@@ -1,14 +1,14 @@
 
 # Analyze the gender distribution of children's book writers and use sound to match names to gender
 
-#import fuzzy
+import fuzzy
 import pandas as pd
 
 # explore the output
-#fuzzy.nysiis('John')
+fuzzy.nysiis('John')
 
 # testing equivalence of similar sounding words
-#fuzzy.nysiis('Jane')
+fuzzy.nysiis('Jane')
 
 # read the files
 author_df = pd.read_csv('datasets/nytkids_yearly.csv', sep=';')
@@ -23,4 +23,4 @@ for name in author_df['Author'] :
 author_df['first_name'] = first_name
 
 # sample the head
-author_df.head()
+#print(author_df.head())
