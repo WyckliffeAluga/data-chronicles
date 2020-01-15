175600d0eb480bf66eebaf2a2866e0a2141229b4
WyckliffeAluga <wyckliffe@culturerobotics.com>
2020-01-15 11:08:38 -0800

    add fuzzy modules

--- Gender Prediction using Sound/script.py
+++ Gender Prediction using Sound/script.py
@@ -1,2 +1,12 @@
 
-# Analyze the gender distribution of children's book writerrs and use sound to match names to gender
+# Analyze the gender distribution of children's book writers and use sound to match names to gender
+
+# importing the modules
+import fuzzy
+import pandas as pd
+
+# explore the output
+fuzzy.nysiis('John')
+
+# testing equivalence of similar sounding words
+fuzzy.nysiis('Jane')
