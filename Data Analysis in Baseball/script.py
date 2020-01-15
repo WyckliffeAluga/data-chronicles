ab594139073682eb3933c75074ea096e58cfe95b
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-01-15 12:14:00 -0800

    Update script.py

--- Data Analysis in Baseball/script.py
+++ Data Analysis in Baseball/script.py
@@ -1,12 +1,4 @@
 
-# here we go
-# in this prokect
-# I will Use MLB's Statcast data to compare New York Yankees sluggers Aaron Judge and Giancarlo Stanton.
-
-# Statcast is a state-of-the-art tracking system that uses high-resolution cameras and radar equipment to measure the precise location and movement of baseballs and baseball players.
-# Introduced in 2015 to all 30 major league ballparks, Statcast data is revolutionizing the game.
-
-# Here I am going to wrangle , visualize and visualize Statcast data to compare two key players
 
 # import some modules
 import pandas as pd
@@ -14,7 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-# laod Aaron Judge's data
+# load Aaron Judge's data
 judge = pd.read_csv('datasets/judge.csv')
 
 # load Giancarlo Stanton's data
