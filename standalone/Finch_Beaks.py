36bc963568b4aeb8007cc9de38be40a2303a9f5b
WyckliffeAluga <wyckliffe@culturerobotics.com>
2020-01-16 15:45:39 -0800

    Update Finch_Beaks.py

--- standalone/Finch_Beaks.py
+++ standalone/Finch_Beaks.py
@@ -533,3 +533,19 @@ print('G. fortis:', heritability_fortis, conf_int_fortis)
 The heritability of beak depth in G. scandens seems low.
 It could be that this observed heritability was just achieved by chance and beak depth is
  actually not really heritable in the species. Let us test this hypothesis by doing paris permutation test """
+
+# Initialize array of replicates: perm_replicates
+perm_replicates = np.empty(10000)
+
+# Draw replicates
+for i in range(10000):
+    # Permute parent beak depths
+    bd_parent_permuted = np.random.permutation(bd_parent_scandens)
+    perm_replicates[i] = heritability(bd_parent_permuted,bd_offspring_scandens)
+
+
+# Compute p-value: p
+p = np.sum(perm_replicates >= heritability_scandens) / len(perm_replicates)
+
+# Print the p-value
+print('p-val =', p)
