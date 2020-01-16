36cd8385ee7bc436dc356e3e70e6aa66872f0ffa
WyckliffeAluga <wyckliffe@culturerobotics.com>
2020-01-16 15:24:55 -0800

    Perform pairs bootstrap for a single statistic.

--- standalone/Finch_Beaks.py
+++ standalone/Finch_Beaks.py
@@ -441,3 +441,26 @@ _ = plt.legend(('G. fortis', 'G. scandens'), loc='lower right')
 
 # Show plot
 plt.show()
+
+"""In an effort to quantify the correlation between offspring and parent beak depths,
+we would like to compute statistics, such as the Pearson correlation coefficient,
+between parents and offspring.
+To get confidence intervals on this, we need to do a pairs bootstrap."""
+
+# define a function to perfom pairs boostrap for a statistic
+def draw_bs_pairs(x, y, func, size=1):
+    """Perform pairs bootstrap for a single statistic."""
+
+    # Set up array of indices to sample from: inds
+    inds = np.arange(len(x))
+
+    # Initialize replicates: bs_replicates
+    bs_replicates = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds, len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_replicates[i] = func(bs_x, bs_y)
+
+    return bs_replicates
