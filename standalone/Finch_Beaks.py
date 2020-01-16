d7348b85af8e5fec0450a2cad33c3359b7bf5c8d
WyckliffeAluga <wyckliffe@culturerobotics.com>
2020-01-16 14:54:19 -0800

    make a plot

--- standalone/Finch_Beaks.py
+++ standalone/Finch_Beaks.py
@@ -168,7 +168,7 @@ _ = plt.ylabel('beak depth (mm)')
 _ = plt.legend(('1975', '2012'), loc='upper left')
 
 # Show the plot
-plt.show()
+#plt.show()
 
 
 # define a function that draws boostraps with linear regreesion
@@ -220,4 +220,31 @@ print('2012: slope =', slope_2012,
 print('2012: intercept =', intercept_2012,
       'conf int =', intercept_conf_int_2012)
 
-print('looks like they have the same slop but different intercepts')
+print('looks like they have the same slope but different intercepts')
+
+# make a plot
+# Make scatter plot of 1975 data
+_ = plt.plot(bl_1975, bd_1975, marker='.',
+             linestyle='none', color='blue', alpha=0.5)
+
+# Make scatter plot of 2012 data
+_ = plt.plot(bl_2012, bd_2012, marker='.',
+             linestyle='none', color='red', alpha=0.5)
+
+# Label axes and make legend
+_ = plt.xlabel('beak length (mm)')
+_ = plt.ylabel('beak depth (mm)')
+_ = plt.legend(('1975', '2012'), loc='upper left')
+
+# Generate x-values for bootstrap lines: x
+x = np.array([10, 17])
+
+# Plot the bootstrap lines
+for i in range(100):
+    plt.plot(x, bs_slope_reps_1975[i] * x + bs_intercept_reps_1975[i] ,
+             linewidth=0.5, alpha=0.2, color='blue')
+    plt.plot(x, bs_slope_reps_2012[i] * x + bs_intercept_reps_2012[i],
+             linewidth=0.5, alpha=0.2, color='red')
+
+# Draw the plot again
+plt.show()
