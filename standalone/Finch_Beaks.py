c6ae3f46934210fca2956080a1e87391cd87cb08
WyckliffeAluga <wyckliffe@culturerobotics.com>
2020-01-16 15:26:35 -0800

    define a function that computes pearson correlation

--- standalone/Finch_Beaks.py
+++ standalone/Finch_Beaks.py
@@ -464,3 +464,12 @@ def draw_bs_pairs(x, y, func, size=1):
         bs_replicates[i] = func(bs_x, bs_y)
 
     return bs_replicates
+
+# define a function that computes pearson correlation
+def pearson_r(x, y):
+    """Compute Pearson correlation coefficient between two arrays."""
+    # Compute correlation matrix: corr_mat
+    corr_mat = np.corrcoef(x,y)
+
+    # Return entry [0,1]
+    return corr_mat[0,1]
