07d7920d75938905478de2594c25b34a5198d0aa
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-01-21 00:00:46 -0800

    Update log_loss.py

--- lib/log_loss.py
+++ lib/log_loss.py
@@ -15,7 +15,7 @@ def compute_log_loss(predicted, actual, eps=1e-14):
     """
 
     predicted = np.clip(predicted, eps , 1 - eps)
-    loss = -1 * np.mean(actual * np.log(predicted)) + (1-actual) * np.log(1-predicted)
+    loss = 1 * np.mean(actual * np.log(predicted)) + (1-actual) * np.log(1 - predicted)
     return loss
 
 
