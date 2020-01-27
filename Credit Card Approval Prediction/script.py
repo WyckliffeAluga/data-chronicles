0c73efa8fcdc59b4b82e19d8b3c70f10d685a907
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-01-27 00:46:38 -0800

    Update script.py

--- Credit Card Approval Prediction/script.py
+++ Credit Card Approval Prediction/script.py
@@ -3,6 +3,11 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
 
 ## load dataset
 applications = pd.read_csv('datasets/cc_approvals.data', header=None)
@@ -58,10 +63,79 @@ Converting all the non-numeric values into numeric ones. This is done
 le = LabelEncoder()
 
   # iterate over alll the values of each column
-  for col in applications.columns:
+for col in applications.columns:
       # compare if the dtype if object
-      if applications[col].dtypes =='object':
-          applications[col] = le.fit_transform(applications[col])
+    if applications[col].dtypes =='object':
+        applications[col] = le.fit_transform(applications[col])
       # use LabelEncoder to do numeric transformation
 
-print(applications.info())
+#print(applications.info())
+
+# drop drivers licence and zipcode
+
+# drop features 11 and 13
+applications = applications.drop(columns=[11,13], axis=1)
+
+# change the dataframe into an array
+applications = applications.values
+
+# separate features from lables
+X, y = applications[:,0:13] , applications[:,13]
+
+
+# split into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+                                X, y,
+                                test_size=0.33,
+                                random_state=42
+)
+
+# instantiate MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+rescladedX_train = scaler.fit_transform(X_train)
+rescaledX_test  = scaler.fit_transform(X_test)
+
+# Instantiate a LogisticRegression classifier with default parameter values
+logreg = LogisticRegression()
+
+# Fit logreg to the train set
+logreg.fit(rescladedX_train, y_train)
+
+# Use logreg to predict instances from the test set and store it
+y_pred = logreg.predict(rescaledX_test)
+
+# Get the accuracy score of logreg model and print it
+print("Accuracy of logistic regression classifier: ", y_pred)
+
+# Print the confusion matrix of the logreg model
+print(confusion_matrix(y_test, y_pred))
+
+
+"""
+For the confusion matrix, the first element of the of the
+first row of the confusion matrix denotes the true negatives
+meaning the number of negative instances (denied applications)
+predicted by the model correctly. And the last element of the second row
+ of the confusion matrix denotes the true positives meaning the number of
+ positive instances (approved applications) predicted by the model correctly. """
+
+ # Define the grid of values for tol and max_iter
+tol = [0.01, 0.001, 0.0001]
+max_iter = [100, 150, 200]
+
+# Create a dictionary where tol and max_iter are keys and the lists of their values are corresponding values
+param_grid = dict({'tol':tol, 'max_iter':max_iter})
+
+# Instantiate GridSearchCV with the required parameters
+grid_model = GridSearchCV(estimator=logreg, param_grid=param_grid, cv=5)
+
+# Use scaler to rescale X and assign it to rescaledX
+rescaledX = scaler.fit_transform(X)
+
+# Fit data to grid_model
+grid_model_result = grid_model.fit(rescaledX, y)
+
+# Summarize results
+best_score = grid_model_result.best_score_
+best_params= grid_model_result.best_params_
+print("Best: %f using %s" % (best_score, best_params))
