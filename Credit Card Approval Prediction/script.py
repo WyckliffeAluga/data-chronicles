898ed3ff49a110b6d3068dc4da7a018e5f71c695
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-01-27 00:12:36 -0800

    Update script.py

--- Credit Card Approval Prediction/script.py
+++ Credit Card Approval Prediction/script.py
@@ -7,4 +7,10 @@ import pandas as pd
 applications = pd.read_csv('datasets/cc_approvals.data', header=None)
 
 # print inspect that data ]
-print(applications.head(5))
+#print(applications.head(5))
+
+"""
+
+As you can see from our first glance at the data, the dataset has a mixture of numerical
+ and non-numerical features. This can be fixed with some preprocessing """
+ 
