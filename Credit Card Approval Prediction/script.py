864ca405a02abe89d480148946eded5c6989037c
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-01-27 00:11:17 -0800

    updated

--- Credit Card Approval Prediction/script.py
+++ Credit Card Approval Prediction/script.py
@@ -1,1 +1,10 @@
 # import modules
+
+import pandas as pd
+
+
+## load dataset
+applications = pd.read_csv('datasets/cc_approvals.data', header=None)
+
+# print inspect that data ]
+print(applications.head(5))
