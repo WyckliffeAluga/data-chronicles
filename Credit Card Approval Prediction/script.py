d01057d38d9a97d43f8bcc05ca742529bb1e7289
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-01-27 00:15:00 -0800

    Update script.py

--- Credit Card Approval Prediction/script.py
+++ Credit Card Approval Prediction/script.py
@@ -13,4 +13,15 @@ applications = pd.read_csv('datasets/cc_approvals.data', header=None)
 
 As you can see from our first glance at the data, the dataset has a mixture of numerical
  and non-numerical features. This can be fixed with some preprocessing """
- 
+
+# let us look at some other properties
+
+# print summary stats
+print(applications.describe()
+print('\n')
+
+# print dataframe information
+print(applications.info())
+
+# inspeact missing values
+print(applications.tail(5))
