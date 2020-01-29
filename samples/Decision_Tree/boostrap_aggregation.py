d21d1a6fdbdc48602746f62a1f226dfb1a87cf47
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-01-29 01:35:45 -0800

    Update boostrap_aggregation.py

--- samples/Decision_Tree/boostrap_aggregation.py
+++ samples/Decision_Tree/boostrap_aggregation.py
@@ -1,5 +1,7 @@
+import pandas as pd
 # Import DecisionTreeClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
 # Import BaggingClassifier
 from sklearn.ensemble import BaggingClassifier
@@ -16,6 +18,16 @@ df = pd.read_csv('indian_liver_patient.csv')
 # get dummies
 df = pd.get_dummies(df, drop_first=True)
 
+
+# split data
+X = df.loc[:, df.columns != 'Dataset'].values
+y = df['Dataset'].values
+
+SEED = 42
+X_train, X_test, y_train , y_test = train_test_split(
+                                                    X, y ,
+                                                    test_size=0.2,
+                                                    random_state=SEED)
 # Fit bc to the training set
 bc.fit(X_train, y_train)
 
