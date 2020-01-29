0ee6a174b81b6f7330d15074f00011008ec5ad36
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-01-29 01:25:31 -0800

    Update boostrap_aggregation.py

---
+++ samples/Decision_Tree/boostrap_aggregation.py
@@ -0,0 +1,27 @@
+# Import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+# Import BaggingClassifier
+from sklearn.ensemble import BaggingClassifier
+
+# Instantiate dt
+dt = DecisionTreeClassifier(random_state=1)
+
+# Instantiate bc
+bc = BaggingClassifier(base_estimator=dt, n_estimators=50, random_state=1)
+
+# load the data
+df = pd.read_csv('indian_liver_patient.csv')
+
+# get dummies
+df = pd.get_dummies(df, drop_first=True)
+
+# Fit bc to the training set
+bc.fit(X_train, y_train)
+
+# Predict test set labels
+y_pred = bc.predict(X_test)
+
+# Evaluate acc_test
+acc_test = accuracy_score(y_pred, y_test)
+print('Test set accuracy of bc: {:.2f}'.format(acc_test))
