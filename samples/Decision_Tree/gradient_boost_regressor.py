550893c31977458e68bea7e961a6e5dfb50e857b
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-01-30 09:36:07 -0800

    Update gradient_boost_regressor.py

--- samples/Decision_Tree/gradient_boost_regressor.py
+++ samples/Decision_Tree/gradient_boost_regressor.py
@@ -22,3 +22,15 @@ rmse_test = mse_test ** (0.5)
 
 # Print RMSE
 print('Test set RMSE of gb: {:.3f}'.format(rmse_test))
+
+# schotastic Gradient boost 
+
+# Import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
+# Instantiate sgbr
+sgbr = GradientBoostingRegressor(max_depth=4, 
+            subsample=0.9,
+            max_features=0.75,
+            n_estimators=200,                                
+            random_state=2)
