f17f699db8dd8a9cab4f5cbb15c22e483418de1c
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-01-30 09:21:01 -0800

    Update gradient_boost_regressor.py

--- samples/Decision_Tree/gradient_boost_regressor.py
+++ samples/Decision_Tree/gradient_boost_regressor.py
@@ -5,3 +5,9 @@ from sklearn.ensemble import GradientBoostingRegressor
 gb = GradientBoostingRegressor(max_depth=4, 
             n_estimators=200,
             random_state=2)
+
+# Fit gb to the training set
+gb.fit(X_train, y_train)
+
+# Predict test set labels
+y_pred = gb.predict(X_test)
