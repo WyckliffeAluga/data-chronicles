4df94b38f0d7b40e2108f9b7b1b002bef46d5176
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-01-30 15:07:21 -0800

    Update data.py

--- Examples/Decision_Tree/data.py
+++ Examples/Decision_Tree/data.py
@@ -10,17 +10,21 @@ class Data(object):
         super(Data, self).__init__()
 
     def dataset(self, name):
-              method_name='df_name_' + name
-              method=getattr(self, method_name, lambda :'Invalid dataset please check the name and try again')
-              return method()
+        method_name='df_name_' + name
+        method=getattr(self, method_name, lambda :'Invalid dataset please check the name and try again')
+        return method()
 
     def df_name_auto(self):
-            df = pd.read_csv('auto.csv')
-            df = pd.get_dummies(df, drop_first=True)
-            return def
-            
+        df = pd.read_csv('auto.csv')
+        df = pd.get_dummies(df, drop_first=True)
+        return df
+
     def df_name_breast_cancer(self):
-             return 'one'
+        df = pd.read_csv('wbc.csv')
+        df = pd.get_dummies(df, drop_first=True)
+        df = df.dropna(axis=1,how='all')
+        return df
+
     def df_name_liver_processed(self):
             return 'two'
     def df_name_liver_unprocessed(self):
