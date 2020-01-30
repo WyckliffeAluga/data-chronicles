5c2786af642e17988b04e420c9cf35f623987d23
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-01-30 08:59:53 -0800

    Update random_forest.py

--- samples/Decision_Tree/random_forest.py
+++ samples/Decision_Tree/random_forest.py
@@ -34,3 +34,15 @@ rmse_test = (MSE(y_test, y_pred))** (0.5)
 
 # Print rmse_test
 print('Test set RMSE of rf: {:.2f}'.format(rmse_test))
+
+# Create a pd.Series of features importances
+importances = pd.Series(data=rf.feature_importances_,
+                        index= X_train.columns)
+
+# Sort importances
+importances_sorted = importances.sort_values()
+
+# Draw a horizontal barplot of importances_sorted
+importances_sorted.plot(kind='barh', color='lightgreen')
+plt.title('Features Importances')
+plt.show()
