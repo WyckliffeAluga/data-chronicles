55f365e81f816a6eef4a7b8e6023ed718f5cbd2e
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-01-30 09:08:28 -0800

    Create adaboost_classifier.py

---
+++ samples/Decision_Tree/adaboost_classifier.py
@@ -0,0 +1,16 @@
+
+"""
+The task is to predict whether a patient suffers from a liver disease using 10 features including Albumin, age and gender.
+"""
+
+# Import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+# Import AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier
+
+# Instantiate dt
+dt = DecisionTreeClassifier(max_depth=2, random_state=1)
+
+# Instantiate ada
+ada = AdaBoostClassifier(base_estimator=dt, n_estimators=180, random_state=1)
