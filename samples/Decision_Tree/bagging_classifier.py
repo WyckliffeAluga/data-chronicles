5dc38ca5b7e72a7b6e1e09e4763d832e27fa16b1
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-01-30 08:43:19 -0800

    Update bagging_classifier.py

--- samples/Decision_Tree/bagging_classifier.py
+++ samples/Decision_Tree/bagging_classifier.py
@@ -12,3 +12,20 @@ bc = BaggingClassifier(base_estimator=dt,
             n_estimators=50,
             oob_score=True,
             random_state=1)
+
+# Fit bc to the training set 
+bc.fit(X_train, y_train)
+
+# Predict test set labels
+y_pred = bc.predict(X_test)
+
+# Evaluate test set accuracy
+acc_test = accuracy_score(y_pred, y_test)
+
+# Evaluate OOB accuracy
+acc_oob = bc.oob_score_
+
+# Print acc_test and acc_oob
+print('Test set accuracy: {:.3f}, OOB accuracy: {:.3f}'.format(acc_test, acc_oob))
+
+# use indian liver patient dataset
