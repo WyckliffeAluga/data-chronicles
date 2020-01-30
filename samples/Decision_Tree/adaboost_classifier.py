a97c05f77348e0d6bdb85011a55425ea23c9cf79
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-01-30 09:11:56 -0800

    Update adaboost_classifier.py

--- samples/Decision_Tree/adaboost_classifier.py
+++ samples/Decision_Tree/adaboost_classifier.py
@@ -20,3 +20,12 @@ ada.fit(X_train, y_train)
 
 # Compute the probabilities of obtaining the positive class
 y_pred_proba = ada.predict_proba(X_test)[:,1]
+
+# Import roc_auc_score
+from sklearn.metrics import roc_auc_score
+
+# Evaluate test-set roc_auc_score
+ada_roc_auc = roc_auc_score(y_test , y_pred_proba)
+
+# Print roc_auc_score
+print('ROC AUC score: {:.2f}'.format(ada_roc_auc))
