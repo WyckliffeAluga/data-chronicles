072366431c9e30af5b013504f94630ef5f92dacb
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-01-30 11:02:31 -0800

    Update random_forest.py

--- samples/Decision_Tree/random_forest.py
+++ samples/Decision_Tree/random_forest.py
@@ -46,3 +46,34 @@ importances_sorted = importances.sort_values()
 importances_sorted.plot(kind='barh', color='lightgreen')
 plt.title('Features Importances')
 plt.show()
+
+# Define the dictionary 'params_rf'
+params_rf = {'n_estimators':[100,350,500], 
+             'max_features':['log2','auto','sqrt'],
+             'min_samples_leaf':[2,10,30]}
+
+# Import GridSearchCV
+from sklearn.model_selection import GridSearchCV
+
+# Instantiate grid_rf
+grid_rf = GridSearchCV(estimator=rf,
+                       param_grid=params_rf,
+                       scoring='neg_mean_squared_error',
+                       cv=3,
+                       verbose=1,
+                       n_jobs=-1)
+
+# Import mean_squared_error from sklearn.metrics as MSE 
+from sklearn.metrics import mean_squared_error as MSE
+
+# Extract the best estimator
+best_model = grid_rf.best_estimator_
+
+# Predict test set labels
+y_pred = best_model.predict(X_test)
+
+# Compute rmse_test
+rmse_test = (MSE(y_test, y_pred)) ** (0.5)
+
+# Print rmse_test
+print('Test RMSE of best model: {:.3f}'.format(rmse_test)) 
