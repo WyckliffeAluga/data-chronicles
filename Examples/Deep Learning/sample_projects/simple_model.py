a96304c91cf18eced59dc6a54eaa9c14c8947e81
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-02 14:51:45 -0800

    Create simple_model.py

---
+++ Examples/Deep Learning/sample_projects/simple_model.py
@@ -0,0 +1,33 @@
+
+
+# Import necessary modules
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+
+# load the dataset
+df = pd.read_csv('datasets/hourly_wages.csv')
+
+predictors = [df.loc[:, df.columns != 'wage_per_hour']].values
+target     = df['wage_per_hour'].values
+
+# Save the number of columns in predictors: n_cols
+n_cols = predictors.shape[1]
+
+# Set up the model: model
+model = Sequential()
+
+# Add the first layer
+model.add(Dense(50, activation='relu', input_shape=(n_cols,)))
+
+# Add the second layer
+model.add(Dense(32, activation='relu'))
+
+# Add the output layer
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Verify that model contains information from compiling
+print("Loss function: " + model.loss)
