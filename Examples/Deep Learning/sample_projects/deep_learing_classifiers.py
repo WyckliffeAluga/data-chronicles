225d331837f9b295f393e953fcf4d20e8f14a26c
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-02 15:05:21 -0800

    Update deep_learing_classifiers.py

--- Examples/Deep Learning/sample_projects/deep_learing_classifiers.py
+++ Examples/Deep Learning/sample_projects/deep_learing_classifiers.py
@@ -21,7 +21,7 @@ model.add(Dense(32, activation='relu', input_shape=(n_cols,)))
 model.add(Dense(2, activation='softmax'))
 
 # Compile the model
-model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics='accuracy')
+model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Fit the model
 model.fit(predictors, target)
