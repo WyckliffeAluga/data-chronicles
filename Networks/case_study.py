67e14605d7e6750bdee522f16b8632766b617f19
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-03 22:43:03 -0800

    Update case_study.py

--- Networks/case_study.py
+++ Networks/case_study.py
@@ -88,5 +88,18 @@ class GitHub(object):
         c.draw()
         plt.show()
 
+    def important_collaborator(self, G):
+        # Compute the degree centralities of G: deg_cent
+        deg_cent = nx.degree_centrality(G)
+
+        # Compute the maximum degree centrality: max_dc
+        max_dc = max(list(deg_cent.values()))
+
+        # Find the user(s) that have collaborated the most: prolific_collaborators
+        prolific_collaborators = [n for n, dc in deg_cent.items() if dc == max_dc]
+
+        # Print the most prolific collaborator(s)
+        print(prolific_collaborators)
+
 g = GitHub()
-g.maximal_clique(G)
+g.important_collaborator(G)
