e9a5cce6b1e19b580053765129eb440e9469d645
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-03 22:51:38 -0800

    Update case_study.py

--- Networks/case_study.py
+++ Networks/case_study.py
@@ -5,6 +5,8 @@ import networkx as nx
 from nxviz import MatrixPlot
 from nxviz.plots import ArcPlot
 from nxviz import CircosPlot
+from itertools import combinations
+from collections import defaultdict
 
 G = pickle.load(open('datasets/github_users.p', 'rb'))
 
@@ -101,5 +103,51 @@ class GitHub(object):
         # Print the most prolific collaborator(s)
         print(prolific_collaborators)
 
+    def communities(self, G):
+
+        # Identify the largest maximal clique: largest_max_clique
+        largest_max_clique = set(sorted(nx.find_cliques(G), key=lambda x: len(x))[-1])
+
+        # Create a subgraph from the largest_max_clique: G_lmc
+        G_lmc = G.subgraph(largest_max_clique).copy()
+
+        # Go out 1 degree of separation
+        for node in list(G_lmc.nodes()):
+            G_lmc.add_nodes_from(G.neighbors(node))
+            G_lmc.add_edges_from(zip([node]*len(list(G.neighbors(node))), G.neighbors(node)))
+
+            # Record each node's degree centrality score
+        for n in G_lmc.nodes():
+            G_lmc.node[n]['degree centrality'] = nx.degree_centrality(G_lmc)[n]
+
+        # Create the ArcPlot object: a
+        a = ArcPlot(G_lmc, node_order='degree centrality')
+
+        # Draw the ArcPlot to the screen
+        a.draw()
+        plt.show()
+
+    def recommend(self, G):
+        # Initialize the defaultdict: recommended
+        recommended = defaultdict(int)
+
+        # Iterate over all the nodes in G
+        for n, d in G.nodes(data=True):
+
+            # Iterate over all possible triangle relationship combinations
+            for n1, n2 in combinations(G.neighbors(n), 2):
+
+                # Check whether n1 and n2 do not have an edge
+                if not G.has_edge(n1, n2):
+
+                    # Increment recommended
+                    recommended[((n1), (n2))] += 1
+
+        # Identify the top 10 pairs of users
+        all_counts = sorted(recommended.values())
+        top10_pairs = [pair for pair, count in recommended if all_counts[-10] > False]
+        print(top10_pairs)
+
+
 g = GitHub()
-g.important_collaborator(G)
+g.recommend(G)
