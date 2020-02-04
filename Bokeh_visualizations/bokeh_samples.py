1cbd9568c165dc1aa843a7460b2b8ca9dfaa1f2e
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-03 23:50:11 -0800

    html generator

--- Bokeh_visualizations/bokeh_samples.py
+++ Bokeh_visualizations/bokeh_samples.py
@@ -2,9 +2,11 @@
 import pandas as pd
 from bokeh.plotting import figure ,ColumnDataSource
 from bokeh.io import output_file , show
+from bokeh.models import HoverTool
 
 auto_df = pd.read_csv('datasets/auto-mpg.csv')
 sprint_df = pd.read_csv('datasets/sprint.csv')
+glucose_df = pd.read_csv('datasets/glucose.csv')
 
 class Visualizations(object):
     """docstring for Visualizations."""
@@ -16,6 +18,7 @@ class Visualizations(object):
             self.df = df
 
     def auto_visualizations(self, df) :
+        # use auto df
          # Create the figure: p
         p = figure(x_axis_label='HP', y_axis_label='MPG')
 
@@ -26,6 +29,7 @@ class Visualizations(object):
         show(p)
 
     def sprint_visualization(self, df):
+        # use sprint df
         # Create a ColumnDataSource from df: source
         source = ColumnDataSource(df)
 
@@ -37,6 +41,37 @@ class Visualizations(object):
         output_file('sprint.html')
         show(p)
 
+    def selection(self, df) :
+        # use sprint df
+        source = ColumnDataSource(df)
+        # Create a figure with the "box_select" tool: p
+        p = figure(x_axis_label='Year', y_axis_label='Time', tools='box_select')
+        # Add circle glyphs to the figure p with the selected and non-selected properties
+        p.circle(x='Year', y='Time', source=source,
+                selection_color='red',
+                nonselection_alpha=0.1)
+        # Specify the name of the output file and show the result
+        output_file('selection_glyph.html')
+        show(p)
+
+    def hovering(self, df):
+        # use glucose_df
+        source = ColumnDataSource(df)
+        p = figure(x_axis_label='Time of the day', y_axis_label='Bllod glucose (mg/dL)')
+        # Add circle glyphs to figure p
+        p.circle(x='datetime', y='glucose', size=10, source=source,
+                fill_color='grey', alpha=0.1, line_color=None,
+                hover_fill_color='firebrick', hover_alpha=0.5,
+                hover_line_color='white')
+        # Create a HoverTool: hover
+        hover = HoverTool(tooltips=None, mode='vline')
+
+        # Add the hover tool to the figure p
+        p.add_tools(hover)
+
+        # Specify the name of the output file and show the result
+        output_file('hover_glyph.html')
+        show(p)
 
 v = Visualizations()
-v.sprint_visualization(sprint_df)
+v.hovering(glucose_df)
