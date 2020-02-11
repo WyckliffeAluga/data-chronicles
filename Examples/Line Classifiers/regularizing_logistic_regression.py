37062fa3f29348e35191c982c3d7e0863d0849e3
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-10 22:38:20 -0800

    Update regularizing_logistic_regression.py

--- Examples/Line Classifiers/regularizing_logistic_regression.py
+++ Examples/Line Classifiers/regularizing_logistic_regression.py
@@ -32,3 +32,20 @@ for C_value in [0.001, 0.01, 0.1, 1, 10, 100, 1000]:
 plt.semilogx(C_values, train_errs, C_values, valid_errs)
 plt.legend(("train", "validation"))
 plt.show()
+
+
+# Specify L1 regularization
+lr = LogisticRegression(penalty='l1')
+
+# Instantiate the GridSearchCV object and run the search
+searcher = GridSearchCV(lr, {'C':[0.001, 0.01, 0.1, 1, 10]})
+searcher.fit(X_train, y_train)
+
+# Report the best parameters
+print("Best CV params", searcher.best_params_)
+
+# Find the number of nonzero coefficients (selected features)
+best_lr = searcher.best_estimator_
+coefs = best_lr.coef_
+print("Total number of features:", coefs.size)
+print("Number of selected features:", np.count_nonzero(coefs))
