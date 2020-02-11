89cb7b4c6324346647314633335456c827a69f33
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-10 22:27:10 -0800

    updates

--- Examples/Line Classifiers/loss_function_diagrams.py
+++ Examples/Line Classifiers/loss_function_diagrams.py
@@ -15,3 +15,20 @@ plt.plot(grid, log_loss(grid), label='logistic')
 plt.plot(grid, hinge_loss(grid), label='hinge')
 plt.legend()
 plt.show()
+
+
+# The logistic loss, summed over training examples
+def my_loss(w):
+    s = 0
+    for i in range(X.shape[0]):
+        raw_model_output = w@X[i]
+        s = s + log_loss(raw_model_output * y[i])
+    return s
+
+# Returns the w that makes my_loss(w) smallest
+w_fit = minimize(my_loss, X[0]).x
+print(w_fit)
+
+# Compare with scikit-learn's LogisticRegression
+lr = LogisticRegression(fit_intercept=False, C=1000000).fit(X,y)
+print(lr.coef_)
---
+++ Examples/Line Classifiers/regularizing_logistic_regression.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+df = read_csv('datasets/indian_liver_patient_preprocessed.csv')
+
+X = df.loc[:, df.columns != 'Liver_disease'].values
+Y = df['Liver_disease'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                    test_size=0.3,
+                                    random_state=42)
+
+# Train and validaton errors initialized as empty list
+train_errs = list()
+valid_errs = list()
+
+# Loop over values of C_value
+for C_value in [0.001, 0.01, 0.1, 1, 10, 100, 1000]:
+    # Create LogisticRegression object and fit
+    lr = LogisticRegression(C=C_value)
+    lr.fit(X_train, y_train)
+
+    # Evaluate error rates and append to lists
+    train_errs.append( 1.0 - lr.score(X_train, y_train) )
+    valid_errs.append( 1.0 - lr.score(X_test, y_test) )
+
+# Plot results
+plt.semilogx(C_values, train_errs, C_values, valid_errs)
+plt.legend(("train", "validation"))
+plt.show()
