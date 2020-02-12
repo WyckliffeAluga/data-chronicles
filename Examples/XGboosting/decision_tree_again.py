b320ed1321d24efe15c9583c63f3b9d21d239981
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-11 23:03:39 -0800

    updates

--- Examples/XGboosting/decision_tree_again.py
+++ Examples/XGboosting/decision_tree_again.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
@@ -14,15 +13,17 @@ y = df['SalePrice'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y,
                                     test_size=0.3,
                                     random_state=42)
-# Instantiate the XGBClassifier: xg_cl
-xg_cl = xgb.XGBClassifier(objective='binary:logistic', n_estimators=10, seed=123)
+
+# Instantiate the classifier: dt_clf_4
+dt_clf_4 = DecisionTreeClassifier(max_depth=4)
 
 # Fit the classifier to the training set
-xg_cl.fit(X_train, y_train)
+dt_clf_4.fit(X_train, y_train)
 
-# Predict the labels of the test set: preds
-preds = xg_cl.predict(X_test)
+# Predict the labels of the test set: y_pred_4
+y_pred_4 = dt_clf_4.predict(X_test)
 
-# Compute the accuracy: accuracy
-accuracy = float(np.sum(preds==y_test))/y_test.shape[0]
-print("accuracy: %f" % (accuracy))
+# Compute the accuracy of the predictions: accuracy
+accuracy = float(np.sum(y_pred_4==y_test))/y_test.shape[0]
+print("accuracy:", accuracy)
+                                    
