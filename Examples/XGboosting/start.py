99c4fb7267644a711caf40ef11bd76e79257f326
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-11 23:01:17 -0800

    Update start.py

--- Examples/XGboosting/start.py
+++ Examples/XGboosting/start.py
@@ -1,10 +1,11 @@
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import xgboost as xgb
 import matplotlib.pyplot as plt
 import numpy as np
 
- df = pd.read_csv('datasets/indian_liver_patient_preprocessed.csv')
+df = pd.read_csv('datasets/ames_housing_trimmed_processed.csv')
 
 X = df.loc[:, df.columns != 'SalePrice'].values
 y = df['SalePrice'].values
@@ -12,3 +13,15 @@ y = df['SalePrice'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y,
                                     test_size=0.3,
                                     random_state=42)
+# Instantiate the XGBClassifier: xg_cl
+xg_cl = xgb.XGBClassifier(objective='binary:logistic', n_estimators=10, seed=123)
+
+# Fit the classifier to the training set
+xg_cl.fit(X_train, y_train)
+
+# Predict the labels of the test set: preds
+preds = xg_cl.predict(X_test)
+
+# Compute the accuracy: accuracy
+accuracy = float(np.sum(preds==y_test))/y_test.shape[0]
+print("accuracy: %f" % (accuracy))
