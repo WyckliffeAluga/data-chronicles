939593dc744c0e89a853ac7c0361bf9ea67ce724
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-11 22:24:13 -0800

    Update multi_class_classifiers.py

--- Examples/Line Classifiers/multi_class_classifiers.py
+++ Examples/Line Classifiers/multi_class_classifiers.py
@@ -64,7 +64,7 @@ svm_small.fit(X_small, y_small)
 pcl.plot_classifier(X_small, y_small, svm_small, lims=(11,15,0,6))
 
 
-# Kernels 
+# Kernels
 # Instantiate an RBF SVM
 svm = SVC()
 
@@ -75,3 +75,34 @@ searcher.fit(X,y)
 
 # Report the best parameters
 print("Best CV params", searcher.best_params_)
+
+
+# Instantiate an RBF SVM
+svm = SVC()
+
+# Instantiate the GridSearchCV object and run the search
+parameters = {'C':[0.1, 1, 10], 'gamma':[0.00001, 0.0001, 0.001, 0.01, 0.1]}
+searcher = GridSearchCV(svm, param_grid=parameters)
+searcher.fit(X_train, y_train)
+
+# Report the best parameters and the corresponding score
+print("Best CV params", searcher.best_params_)
+print("Best CV accuracy", searcher.best_score_)
+
+# Report the test accuracy using these best parameters
+print("Test accuracy of best grid search hypers:", searcher.score(X_test, y_test))
+
+from sklearn.linear_model import SGDClassifier
+# We set random_state=0 for reproducibility
+linear_classifier = SGDClassifier(random_state=0)
+
+# Instantiate the GridSearchCV object and run the search
+parameters = {'alpha':[0.00001, 0.0001, 0.001, 0.01, 0.1, 1],
+             'loss':['hinge','log'], 'penalty':['l1','l2']}
+searcher = GridSearchCV(linear_classifier, param_grid=parameters, cv=10)
+searcher.fit(X_train, y_train)
+
+# Report the best parameters and the corresponding score
+print("Best CV params", searcher.best_params_)
+print("Best CV accuracy", searcher.best_score_)
+print("Test accuracy of best grid search hypers:", searcher.score(X_test, y_test))
