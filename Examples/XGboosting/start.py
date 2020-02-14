e666aabf5b651c19f5f7993aa474ec759008993d
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-14 15:59:56 -0800

    Update start.py

--- Examples/XGboosting/start.py
+++ Examples/XGboosting/start.py
@@ -26,3 +26,24 @@ preds = xg_cl.predict(X_test)
 # Compute the accuracy: accuracy
 accuracy = float(np.sum(preds==y_test))/y_test.shape[0]
 print("accuracy: %f" % (accuracy))
+
+
+# Create arrays for the features and the target: X, y
+X, y = churn_data.iloc[:,:-1], churn_data.iloc[:,-1]
+
+# Create the DMatrix from X and y: churn_dmatrix
+churn_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary: params
+params = {"objective":"reg:logistic", "max_depth":3}
+
+# Perform cross-validation: cv_results
+cv_results = xgb.cv(dtrain=churn_dmatrix, params=params,
+                  nfold=3, num_boost_round=5,
+                  metrics="error", as_pandas=True, seed=123)
+
+# Print cv_results
+print(cv_results)
+
+# Print the accuracy
+print(((1-cv_results["test-error-mean"]).iloc[-1]))
