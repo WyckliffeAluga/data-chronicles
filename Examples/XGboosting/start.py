246d275d0adf1cf364ef082bae0c0d679846f8b7
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-14 16:03:53 -0800

    Update start.py

--- Examples/XGboosting/start.py
+++ Examples/XGboosting/start.py
@@ -47,3 +47,15 @@ print(cv_results)
 
 # Print the accuracy
 print(((1-cv_results["test-error-mean"]).iloc[-1]))
+
+
+# Perform cross_validation: cv_results
+cv_results = xgb.cv(dtrain=churn_dmatrix, params=params,
+                  nfold=3, num_boost_round=5,
+                  metrics="auc", as_pandas=True, seed=123)
+
+# Print cv_results
+print(cv_results)
+
+# Print the AUC
+print((cv_results["test-auc-mean"]).iloc[-1])
