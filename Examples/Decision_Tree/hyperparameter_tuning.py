53fc087791408b4b3e85cca771e652b39c4df064
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-20 00:12:25 -0500

    Update hyperparameter_tuning.py

--- Examples/Decision_Tree/hyperparameter_tuning.py
+++ Examples/Decision_Tree/hyperparameter_tuning.py
@@ -1,10 +1,28 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+
+# load the data
+df = pd.read_csv('datasets/indian_liver_patient_preprocessed.csv')
+
+# split data
+X = df.loc[:, df.columns != 'Liver_disease'].values
+y = df['Liver_disease'].values
+
+SEED = 42
+X_train, X_test, y_train , y_test = train_test_split(
+                                                    X, y ,
+                                                    test_size=0.3,
+                                                    random_state=SEED
+)
+# Instantiate dt
+dt = DecisionTreeClassifier(min_samples_leaf=0.13, random_state=SEED)
 # Define params_dt
-params_dt = {'max_depth': [2,3,4] , 
+params_dt = {'max_depth': [2,3,4] ,
             'min_samples_leaf':[0.12,0.14,0.16,0.18]
 }
-
-# Import GridSearchCV
-from sklearn.model_selection import GridSearchCV
 
 # Instantiate grid_dt
 grid_dt = GridSearchCV(estimator=dt,
@@ -12,11 +30,9 @@ grid_dt = GridSearchCV(estimator=dt,
                        scoring='roc_auc',
                        cv=5,
                        n_jobs=-1)
-                       
+
 grid_dt.fit(X_train, y_train)
 
-# Import roc_auc_score from sklearn.metrics 
-from sklearn.metrics import roc_auc_score
 
 # Extract the best estimator
 best_model = grid_dt.best_estimator_
