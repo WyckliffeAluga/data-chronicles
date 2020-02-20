1ed73d981cb9f8c170004f6c129840757770ec6c
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-20 00:08:35 -0500

    Update gradient_boost_regressor.py

--- Examples/Decision_Tree/gradient_boost_regressor.py
+++ Examples/Decision_Tree/gradient_boost_regressor.py
@@ -1,8 +1,25 @@
 # Import GradientBoostingRegressor
 from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error as MSE
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor
+import pandas as pd
 
+
+# load the data
+df = pd.read_csv('datasets/indian_liver_patient_preprocessed.csv')
+
+# split data
+X = df.loc[:, df.columns != 'Liver_disease'].values
+y = df['Liver_disease'].values
+
+X_train, X_test, y_train , y_test = train_test_split(
+                                                    X, y ,
+                                                    test_size=0.3,
+                                                    random_state=42
+)
 # Instantiate gb
-gb = GradientBoostingRegressor(max_depth=4, 
+gb = GradientBoostingRegressor(max_depth=4,
             n_estimators=200,
             random_state=2)
 
@@ -12,8 +29,6 @@ gb.fit(X_train, y_train)
 # Predict test set labels
 y_pred = gb.predict(X_test)
 
-# Import mean_squared_error as MSE
-from sklearn.metrics import mean_squared_error as MSE
 # Compute MSE
 mse_test = MSE(y_test, y_pred)
 
@@ -23,16 +38,14 @@ rmse_test = mse_test ** (0.5)
 # Print RMSE
 print('Test set RMSE of gb: {:.3f}'.format(rmse_test))
 
-# schotastic Gradient boost 
+# schotastic Gradient boost
 
-# Import GradientBoostingRegressor
-from sklearn.ensemble import GradientBoostingRegressor
 
 # Instantiate sgbr
-sgbr = GradientBoostingRegressor(max_depth=4, 
+sgbr = GradientBoostingRegressor(max_depth=4,
             subsample=0.9,
             max_features=0.75,
-            n_estimators=200,                                
+            n_estimators=200,
             random_state=2)
 # Fit sgbr to the training set
 sgbr.fit(X_train, y_train)
@@ -40,8 +53,6 @@ sgbr.fit(X_train, y_train)
 # Predict test set labels
 y_pred = sgbr.predict(X_test)
 
-# Import mean_squared_error as MSE
-from sklearn.metrics import mean_squared_error as MSE
 
 # Compute test set MSE
 mse_test = MSE(y_test, y_pred)
