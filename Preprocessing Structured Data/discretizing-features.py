6f0d1abb89cccabd39881bf008db7dfbbb3b227c
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-26 15:50:31 -0800

    Create discretizing-features.py

---
+++ Preprocessing Structured Data/discretizing-features.py
@@ -0,0 +1,15 @@
+# load libraries
+from sklearn.preprocessing import Binarizer
+import numpy as np
+
+# Create feature
+age = np.array([[6],
+                [12],
+                [20],
+                [36],
+                [65]])
+
+# Create binarizer
+binarizer = Binarizer(18)
+# Transform feature
+binarizer.fit_transform(age)
