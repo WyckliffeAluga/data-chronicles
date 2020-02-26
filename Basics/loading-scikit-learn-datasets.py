0dc3f08ed24be77a1ff63187e85db78c572d4e47
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-26 14:12:51 -0800

    Create loading-scikit-learn-datasets.py

---
+++ Basics/loading-scikit-learn-datasets.py
@@ -0,0 +1,17 @@
+# load libraries
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+# load digits dataset
+boston = datasets.load_boston()
+
+# create a feature matrix
+x = boston.data
+
+# create a target vector
+y = boston.target
+
+# display features value for the first observation as floats
+features = ['{:f}'.format(x) for x in x[0]]
+
+print(features)
