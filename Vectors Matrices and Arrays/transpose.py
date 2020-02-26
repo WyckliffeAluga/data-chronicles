9ed896ca6683a8d2e52eb1202f17667bbf043140
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-26 15:14:34 -0800

    Create transpose.py

---
+++ Vectors Matrices and Arrays/transpose.py
@@ -0,0 +1,17 @@
+# load numpy
+import numpy as np
+
+# create a vector
+vector = np.array([1,2,3,4,5,6])
+
+# create a matrix
+matrix = np.array([[1,2,3],
+                  [4,5,6],
+                  [7,8,9]])
+# transposer vector
+transpose = vector.T
+
+# transpose matrix
+transpose = matrix.T
+
+print(transpose)
