b4f4f61d02cb1e6e24e439f5bdc8ae41497d5d41
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-26 22:42:47 -0800

    Create cropping-images.py

---
+++ Preprocessing Images/cropping-images.py
@@ -0,0 +1,14 @@
+# Load image
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Load image as grayscale
+image = cv2.imread('images/plane.jpg', cv2.IMREAD_GRAYSCALE)
+
+# Select first half of the columns and all rows
+image_cropped = image[:,:126]
+
+# View image
+plt.imshow(image_cropped, cmap='gray'), plt.axis("off")
+plt.show()
