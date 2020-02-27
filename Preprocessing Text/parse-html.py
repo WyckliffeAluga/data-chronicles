c96e8f5f429d6af12944ce65754d6298e8388306
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-26 23:06:35 -0800

    Update parse-html.py

---
+++ Preprocessing Text/parse-html.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+
+
+# Create some HTML code
+html = "<div class='full_name'><span style='font-weight:bold'>Masego</span> Azra</div>"
+
+# Parse html
+soup = BeautifulSoup(html, "lxml")
+
+# Find the div with the class "full_name", show text
+soup.find("div", { "class" : "full_name" }).text
