8258f828f3ac65c26916e6774fb350092b6b3860
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-26 22:26:45 -0800

    Create standardize-a-feature.py

---
+++ Preprocessing Structured Data/standardize-a-feature.py
@@ -0,0 +1,16 @@
+# load libraries
+from sklearn import preprocessing
+import numpy as np
+
+# Create feature
+x = np.array([[-500.5],
+              [-100.1],
+              [0],
+              [100.1],
+              [900.9]])
+              
+# Create scaler
+scaler = preprocessing.StandardScaler()
+
+# Transform the feature
+standardized = scaler.fit_transform(x)
