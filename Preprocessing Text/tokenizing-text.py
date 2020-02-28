52f0163162788728df6713789b903f4d21f4075c
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-27 19:46:01 -0800

    more

---
+++ Preprocessing Text/tokenizing-text.py
@@ -0,0 +1,12 @@
+
+# load libraries
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+# create text
+string = "The science of today is the technology of tomorrow. Tomorrow is today."
+
+# tokenize the words
+word_tokenize(string)
+
+# tokenize sentences
+sent_tokenize(string)
