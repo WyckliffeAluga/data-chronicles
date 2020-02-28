e8af9664f107bc2413a7f64123d957e7eef01612
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-27 20:29:17 -0800

    Create accuracy.py

---
+++ Model Evaluation/accuracy.py
@@ -0,0 +1,19 @@
+
+# Load libraries
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import make_classification
+
+# Generate features matrix and target vector
+X, y = make_classification(n_samples = 10000,
+                           n_features = 3,
+                           n_informative = 3,
+                           n_redundant = 0,
+                           n_classes = 2,
+                           random_state = 1)
+
+# Create logistic regression
+lg = LogisticRegression()
+
+# Cross-validate model using accuracy
+cross_val_score(lg, X, y, scoring="accuracy")
