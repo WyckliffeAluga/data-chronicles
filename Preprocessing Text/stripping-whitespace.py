f0a128b59542e71a45669cd8102dd2f7ebcc2d77
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-02-27 19:37:15 -0800

    Update stripping-whitespace.py

---
+++ Preprocessing Text/stripping-whitespace.py
@@ -0,0 +1,10 @@
+
+# Create text
+text_data = ['   Interrobang. By Aishwarya Henriette     ',
+             'Parking And Going. By Karl Gautier',
+             '    Today Is The night. By Jarek Prakash   ']
+
+# strip whitespaces
+strip_whitespace = [string.strip() for string in text_data]
+
+strip_whitespace
